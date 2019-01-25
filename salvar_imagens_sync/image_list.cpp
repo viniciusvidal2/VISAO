d21f13bd02474d47c21b2a83d3648726db15802e
vinicius <vinicius.vidal@engenharia.ufjf.br>
2019-01-24 23:34:23 -0200

    Agora republicando imagens para menor resolucao na mao

--- salvar_imagens_sync/image_list.cpp
+++ salvar_imagens_sync/image_list.cpp
@@ -12,12 +12,31 @@
 #include <message_filters/time_synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
 
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <opencv2/features2d.hpp>
+#include <opencv2/xfeatures2d.hpp>
+#include <opencv2/flann.hpp>
+#include <opencv2/stereo.hpp>
+
+#include <opencv2/calib3d.hpp>
+
 using namespace message_filters;
 typedef sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> syncPolicy;
 
 int ordem = 0;
 std::string camera = "right";
 std::string nome   = "pitch";
+
+image_transport::Publisher pub_left;
+image_transport::Publisher pub_right;
+sensor_msgs::Image left;
+sensor_msgs::Image right;
+
 void leftCallback(const sensor_msgs::ImageConstPtr& msg)
 {
   cv_bridge::CvImagePtr cv_ptr;
@@ -48,13 +67,21 @@ void cameras_callback(const sensor_msgs::ImageConstPtr& left, const sensor_msgs:
     ROS_ERROR("cv_bridge exception: %s", e.what());
     return;
   }
-  cv::imwrite("/home/vinicius/stereo/left/" +boost::lexical_cast<std::string>(ordem)+".jpg",  left_cv->image);
-  cv::imwrite("/home/vinicius/stereo/right/"+boost::lexical_cast<std::string>(ordem)+".jpg", right_cv->image);
-  std::cout << "Gravamos as imagens da posicao " << ordem << std::endl;
-  ordem++;
 
-  ros::Rate rate(1);
-  rate.sleep();
+  cv::resize(left_cv->image , left_cv->image , cv::Size(), 0.5, 0.5, cv::INTER_AREA);
+  cv::resize(right_cv->image, right_cv->image, cv::Size(), 0.5, 0.5, cv::INTER_AREA);
+
+  pub_left.publish(left_cv->toImageMsg());
+  pub_right.publish(right_cv->toImageMsg());
+
+  std::cout << "\nTamanho da imagem: " << left_cv->image.cols << "\t" << left_cv->image.rows << std::endl;
+//  cv::imwrite("/home/vinicius/stereo/left/" +boost::lexical_cast<std::string>(ordem)+".jpg",  left_cv->image);
+//  cv::imwrite("/home/vinicius/stereo/right/"+boost::lexical_cast<std::string>(ordem)+".jpg", right_cv->image);
+//  std::cout << "Gravamos as imagens da posicao " << ordem << std::endl;
+//  ordem++;
+
+//  ros::Rate rate(1);
+//  rate.sleep();
 }
 
 int main(int argc, char **argv)
@@ -62,6 +89,10 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "image_list");
   ros::NodeHandle nh;
   image_transport::ImageTransport it(nh);
+
+  // Publicadores se quiser so reduzir a imagem e repassar
+  pub_left  = it.advertise("/stereo/left/image_raw1" , 1);
+  pub_right = it.advertise("/stereo/right/image_raw1", 1);
 
 //  image_transport::Subscriber sub_left  = it.subscribe("/stereo/"+camera+"/image_rect_color",  1000, leftCallback);
   Subscriber<sensor_msgs::Image> subl(nh, "/stereo/left/image_raw" , 100);
